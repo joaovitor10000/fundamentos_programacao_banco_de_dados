e5f07e0b2f298e71a19629e7cd1a1b81ab8cf672
João vitor <147009431+joaovitor10000@users.noreply.github.com>
2024-06-20 19:01:17 -0300

    Update scrpt.js

--- Calculadora - João/scrpt.js
+++ Calculadora - João/scrpt.js
@@ -1,17 +1,21 @@
+//Aqui estamos encontrando e guardando a informação do elemento na página
 let display = document.getElementById('display');
 
+// Aqui vai funcionar o botão dos números 
 function appendToDisplay(value) {
     display.value += value;
 }
 
+// Aqui ele vai limpar a calculadora quando você Apertar a letra C
 function clearDisplay() {
     display.value = '';
 }
 
+// Aqui ele vai calcular o valor da sua conta
 function calculate() {
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value); 
     } catch (error) {
-        display.value = 'Error';
+        display.value = 'Error'; // Aqui ele vai exibir um erro caso você faça errado 
     }
 }
