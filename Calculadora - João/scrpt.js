f6eb2adfebb853d8f2f23402a8cb0edd0391e0c5
João vitor <147009431+joaovitor10000@users.noreply.github.com>
2024-06-20 15:56:32 -0300

    Update scrpt.js

--- Calculadora - João/scrpt.js
+++ Calculadora - João/scrpt.js
@@ -1,77 +1,17 @@
-var input = document.querySelector("#display");
-var operador = "";
-var parar = false;
+let display = document.getElementById('display');
 
-function limpar() {
-  input.value = "";
-  document.querySelector("#history").innerHTML = "";
-  operador = ""
+function appendToDisplay(value) {
+    display.value += value;
 }
 
-function add_numero(string) {
-  if (!parar) {
-    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
-      input.value = input.value + string;
-      history.value = input.value;
-    }
-  } else {
-    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
-      input.value = "";
-      parar = false;
-      document.querySelector("#history").innerHTML = "";
-      operador = "";
-
-      input.value = input.value + string;
-    }
-  }
+function clearDisplay() {
+    display.value = '';
 }
 
-function add_operador(op) {
-  if (input.value == "") {
-    input.value = "0";
-  }
-
-  if (operador == "") {
-    operador = op;
-    input.value = input.value + op;
-  } else {
-    total()
-    input.value = input.value + op;
-    operador = op;
-    parar=false;
-  }
-}
-
-function total() {
-  if (!parar) {
-    numeros = input.value.split(operador);
-
-    if (numeros[1] == "") {
-      input.value = input.value + '0';
-      numeros[1] = 0;
-
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
     }
-
-    document.querySelector("#history").innerHTML = input.value;
-
-    switch (operador) {
-      case "+":
-        input.value = Number(numeros[0]) + Number(numeros[1]);
-        break;
-
-      case "-":
-        input.value = numeros[0] - numeros[1];
-        break;
-
-      case "*":
-        input.value = numeros[0] * numeros[1];
-        break;
-
-      case "/":
-        input.value = numeros[0] / numeros[1];
-        break;
-    }
-
-    parar = true;
-  }
 }
