b09d5b851115fbb2b48836236459824d229db03e
João vitor <147009431+joaovitor10000@users.noreply.github.com>
2024-07-03 21:38:29 -0300

    Add files via upload

---
+++ João Vitor - python/condicional.py
@@ -0,0 +1,27 @@
+#Data 03/07/24
+#print ('Olá mundo!!!')
+
+#idade = 16
+
+#if idade == 16:
+   # print('Maior de idade') 
+
+#idade = int(input ('Digite a sua Idade: '))
+
+#if idade < 16:
+   # print ('Você é Menor de Idade')
+#else:
+   # print ('Você é maoir de Idade')
+
+
+fetaria = int(input('Digite Sua idade: '))
+
+if  fetaria  > 18 :
+    print ('Filme de terror:')
+
+elif fetaria >=14   :
+    print ('Filme de ação')
+
+else :
+    print ('Filme Infatil')
+
---
+++ João Vitor - python/lancoderepeticao.py
@@ -0,0 +1,18 @@
+#Outra atividade 03/07/24
+#x=0
+
+#while x < 5:
+    #nome = input('Qual o seu nome? ')
+    #x=x+1
+
+x=0
+animais =[]
+
+while x < 5:
+    animal =input ('5 Animais que começe com a letra do seu nome? ')
+    animais.append(animal)
+    x=x+1
+
+    print(animais)
+
+
