5c97d38bf5a7b58e17dc39cd0c5dd7cd21295ac5
João vitor <147009431+joaovitor10000@users.noreply.github.com>
2024-07-16 20:59:20 -0300

    Add files via upload

--- João Vitor - python/avaliacao-joao.py
+++ João Vitor - python/avaliacao-joao.py
@@ -3,29 +3,29 @@ print ('Avaliação')
 
 
 
-#valor_compra=append.(valor_compra):
-#if valor_compra <= 100:
-     #desconto =0
-#preco_final = valor_compra 
-#elif valor_compra <=200:
- #desconto = 10
-   # preco_final = valor_compra * (1 - desconto)
-#elif valor_compra <= 300:
- #desconto = 15
-    #preco_final = valor_compra *   (1 - desconto)
-#else:
-# desconto = 30
-    #preco_final = valor_compra *   ( 1 - desconto)
+valor_compra=append.(valor_compra):
+if valor_compra <= 100:
+     desconto =0
+preco_final = valor_compra 
+elif valor_compra <=200:
+ desconto = 10
+    preco_final = valor_compra * (1 - desconto)
+elif valor_compra <= 300:
+ desconto = 15
+    preco_final = valor_compra *   (1 - desconto)
+else:
+ desconto = 30
+    preco_final = valor_compra *   ( 1 - desconto)
 
-#return preco_final
+return preco_final
 
-#calcular_preco_final
-#valor_compra = float(input("digite o valor total da compra : R$"))
-#preco_final = calcular_preco_final (valor_compra)
+calcular_preco_final
+valor_compra = float(input("digite o valor total da compra : R$"))
+preco_final = calcular_preco_final (valor_compra)
 
 
-#if __name__ =="__main__":
-    #main()
+if __name__ =="__main__":
+    main()
 
 
 
---
+++ João Vitor - python/funcao.py
@@ -0,0 +1,47 @@
+#Funcões 164 pagina, Pre definição uma atividade ou rotina dentro do python,input ou if else...
+#print vem com uma mensagem de texto e ele auto exxecuta por tela...
+
+#def mensagem():
+##    print('Seja bem vindo Javascrpit')
+#
+##mensagem()
+
+
+##def mensagem():
+  ##  print('Lembrete, caro clinte não esqueça de pagar!')
+
+##def mensagem2(clinte):
+   ## print('Lembrete, claro ' +clinte+ ' não esqueça de pagar ' '!')
+
+##mensagem2('João')
+
+
+
+def mulher():
+    print('alta')
+    print('Magra')
+    print('Cabelo curto')
+
+altura = input ('Qual a sua altura? ')
+peso   = input ('Qual o seu peso? ')
+Tamanhodocabelo = input ('Qual o tamanho do seu cabelo? ')
+
+
+if altura:
+   print('Você é Alta')
+else:
+   print('Você é Baixa')
+
+if peso:
+   print('Você é Magra')
+else:
+   print('Você é Gorda')
+
+if Tamanhodocabelo:
+   print('Seu cabelo é Curto')
+else:
+   print('Seu cabelo é Longo')
+   
+
+   
+   
