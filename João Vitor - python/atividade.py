dc5894d86232eb92c6272b3702c7180f7b44bcd9
João vitor <147009431+joaovitor10000@users.noreply.github.com>
2024-07-16 21:09:51 -0300

    Add files via upload

---
+++ João Vitor - python/atividade.py
@@ -0,0 +1,13 @@
+def media_aluno(nota1,nota2,nota3):
+    media = ((nota1+nota2+(nota3*2))/4)
+    return(media)
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a nota do trabalho: '))
+
+print('')
+
+media_final  = media_aluno(n1,n2,n3)
+
+print ('A sua media final do aluno é:',media_final)
